47705b7a6f00cb3c3cc154ad9058ef92026585c6
Your Name <you@example.com>
2023-12-06 01:34:01 +0500

    Еще фронта

--- src/s21_smartcalc/s21_credit.cpp
+++ src/s21_smartcalc/s21_credit.cpp
@@ -13,6 +13,24 @@ s21_credit::s21_credit(QWidget *parent)
     ui->creditBox->addItem("Инженерный");
     ui->creditBox->addItem("Депозитный");
 
+    ui->calc_type_box->addItem("Расчет ежемесячного платежа");
+    ui->calc_type_box->addItem("Расчет срока кредита");
+    ui->calc_type_box->addItem("Расчет максимальной суммы кредита");
+
+    ui->sum_box->addItem(" ₽ - RUB");
+    ui->sum_box->addItem(" $ - USD");
+    ui->sum_box->addItem(" € - EUR");
+    ui->sum_box->addItem(" ¥ - CNY");
+    ui->sum_box->addItem(" ₹ - INR");
+    ui->sum_box->addItem(" ₸ - KZT");
+    ui->sum_box->addItem(" ¥ - JPY");
+    ui->sum_box->addItem(" ₣ - CHF");
+    ui->sum_box->addItem(" ฿ - THB");
+    ui->sum_box->addItem(" ₾ - GEL");
+
+    ui->time_box->addItem("лет");
+    ui->time_box->addItem("месяцев");
+
     connect(ui->creditBox, SIGNAL(activated(int)), this, SLOT(change_credit(int)));
 }
 
